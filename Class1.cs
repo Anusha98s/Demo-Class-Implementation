322f93bffac9720606f969aa007e6f14bd8376d1
Anusha98s <92158408+Anusha98s@users.noreply.github.com>
2021-10-14 10:31:27 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Class_Implementation
+{
+   partial class LMS : IAsessment
+   {
+        
+        string name;
+        int SNO;
+        long mobilenum;
+        string email;
+
+        public void Display()
+        void IAsessment.MCQ_Asessment()
+
+            void IAsessment.QuestionBank()
+        {
+
+        }
+
+        
+       
+
+   }
+    partial class LMS : Placement, Asessment //inheritance
+    {
+        public void Message()
+        {
+
+            Console.WriteLine("This Message is coming from second implementation of class");
+        }
+
+
+    }
+    abstract class Placement
+    {
+        string Name_Of_Company;
+        int No_Of_Student_Placed;
+        DateTime Year_Of_Placement;
+
+       public void Placement_record()
+        {
+            Console.WriteLine("This method will show you complete record of the placement");
+        }
+    }
+     interface IAsessment
+    {
+
+        //int Asessment_Count; creating instance field is not allowed
+
+        public void MCQ_Asessment();
+
+        public void QuestionBank();
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Demo_Class_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LMS Anu = new LMS();
+            Anu.Display();
+            Anu.Message();
+            Anu.Placement_record();
+            Asessment Delhi;
+            Delhi.MCQ_Asessment();
+            Delhi.QuestionBank();
+
+
+
+            //Placement NIT = new Placement();
+
+        }
+    }
+}
